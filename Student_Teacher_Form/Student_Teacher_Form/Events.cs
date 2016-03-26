ed76af49208df35542ca75a0dc5d49d13c1342c6
Barış Atış <dhephaistos@gmail.com>
2016-03-26 15:11:36 +0200

    Well

---
+++ Student_Teacher_Form/Student_Teacher_Form/Events.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Teacher_Form
+{
+    class Events
+    {
+        private int id;
+        private String name, info, organizer, place;
+
+        
+    }
+}
--- Student_Teacher_Form/Student_Teacher_Form/Msgr.cs
+++ Student_Teacher_Form/Student_Teacher_Form/Msgr.cs
@@ -10,5 +10,28 @@ namespace Student_Teacher_Form
     {
         private int id;
         private String name;
+
+        public Msgr(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public Msgr(string name)
+        {
+            this.name = name;
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
     }
 }
