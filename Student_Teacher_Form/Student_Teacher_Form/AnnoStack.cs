6ca7ac37e6ff470df7df9ca0de6c326bfeecfeeb
Barış Atış <dhephaistos@gmail.com>
2016-04-26 18:24:59 +0300

    WEait

---
+++ Student_Teacher_Form/Student_Teacher_Form/AnnoStack.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Teacher_Form
+{
+    class AnnoStack
+    {
+        private AnnoNode top;
+
+        public AnnoNode Top
+        {
+            get { return top; }
+            set { top = value; }
+        }
+    }
+}
